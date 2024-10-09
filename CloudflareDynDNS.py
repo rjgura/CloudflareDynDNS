664f0a105702b597854e99463a8a7f5612f96187
R.J. Gura <rjgura@gmail.com>
2024-10-09 04:08:32 -0400

    Code now runs update asynchronously for all listed domains

--- CloudflareDynDNS.py
+++ CloudflareDynDNS.py
@@ -33,7 +33,7 @@ logger = logging.getLogger('logger')
 logger.setLevel(logging.DEBUG)
 
 sh = logging.StreamHandler(sys.stdout)
-sh.setLevel(logging.INFO)
+sh.setLevel(logging.DEBUG)
 sh.setFormatter(FORMATTER)
 logger.addHandler(sh)
 
@@ -49,6 +49,9 @@ fh.setLevel(logging.DEBUG)
 fh.setFormatter(FORMATTER)
 logger.addHandler(fh)
 
+logger.debug(f'Beginning a new run of CloudflareDynDNS.py')
+# Check if running on a linux system, but not from terminal
+# This fix stops output going to an unattached terminal if being run by cron
 if sys.platform[0:5] == 'linux' and not sys.stdout.isatty():
     logger.removeHandler(sh)
 
@@ -58,12 +61,16 @@ if sys.platform[0:5] == 'linux' and not sys.stdout.isatty():
 config = configparser.ConfigParser()
 config.read(CONFIG_PATH)
 
+logger.debug(f'Loading config file: {CONFIG_PATH}')
 try:
-    LAST_RECORDED_IP = config.items('IP_ADDRESSES')
+    last_recorded_ip = config['IP_ADDRESSES']['Last_Recorded_IP']
     DOMAINS_LIST = config.items('DOMAINS_LIST')
-    RECORD_TYPE = config['RECORD']['Record_Type']
-    RECORD_NAME = config['RECORD']['Record_Name']
+    RECORD_TYPE = config['RECORD_TYPE']['Record_Type']
+    RECORD_NAME = config['RECORD_TYPE']['Record_Name']
     CLOUDFLARE_API_TOKEN = config['CREDENTIALS']['Cloudflare_API_Token']
+
+ # TODO: Catch error when missing a section
+ # TODO: Catch error when missing an entry
 
 except KeyError:
     logger.error('Error loading ini: check ini exists and settings are correct')
@@ -71,56 +78,64 @@ except KeyError:
 
 try:
     logger.debug('Getting External IP')
-    externalIP = get_public_ip()
-    logger.info('Got External IP: ' + externalIP)
+    external_ip = get_public_ip()
+    logger.info(f'Got External IP: {external_ip}')
 
 except Exception as e:
-    logger.error('Error Getting External IP: ' + e.__str__())
+    logger.error(f'Error Getting External IP: {e.__str__()}')
     sys.exit()
 
-if externalIP != LAST_RECORDED_IP['last_recorded_IP']:
+if external_ip != last_recorded_ip:
+    logger.debug(f'Creating new AsyncCloudflare client')
     client = AsyncCloudflare(api_token=CLOUDFLARE_API_TOKEN)
+    # TODO: Catch error if no network
+    # TODO: Catch rate limit error
 
     async def update_a_record(domain, new_ip):
         try:
-            logger.debug(f'fGetting Cloudflare Records for {domain}')
-            zones = await client.zones.list(name=domain)
+            # Get zone ID for domain from Cloudflare
+            logger.debug(f'Getting Cloudflare Records for {domain}')
+            zones = [zone async for zone in client.zones.list(name=domain)]
             if not zones:
                 logger.debug(f'Zone not found for {domain}')
                 return
-            zone_id = zones[0]['id']
+            zone_id = zones[0].id
+            logger.debug(f'Zone ID for {domain}: {zone_id}')
 
-            records = await client.dns.records.list(zone_id=zone_id, type="A", name=domain)
+            logger.debug(f'Getting DNS Records List for Zone ID: {zone_id}')
+            records = [record async for record in client.dns.records.list(zone_id=zone_id, type="A", name=domain)]
             if not records:
                 logger.info(f'No A records found for {domain}')
                 return
 
+            for record in records:
+                record_id = record.id
+
+                logger.debug(f'Updating IP Address for DNS Record ID: {record_id}')
+                await client.dns.records.update(
+                    zone_id=zone_id,
+                    name=RECORD_NAME,
+                    dns_record_id=record_id,
+                    type="A",
+                    content=new_ip
+                )
+                logger.debug(f'Updated A record for {domain} to {new_ip}')
+                logger.info(f'DNS Record Updated for {domain} to {external_ip}')
+
         except Exception as e:
             logger.error(f'Error updating A record for {domain}: {e}')
 
-            for record in records:
-                record_id = record['dns_record_id']
-                await client.dns.records.update(
-                    zone_id=zone_id,
-                    dns_record_id=record_id,
-                    type="A",
-                    content=new_ip,
-                )
-           #       logger.debug(f'Updated A record for {domain} to {new_ip}')
-           #       logger.info('DNS Record Updated for ' + DOMAIN[1] + ':' + record["data"] + ' to ' + externalIP)
+        # except Exception as e:
+        #     logger.error(f'Error Trying to Update DNS Record: {e.__str__()}')
+        #     sys.exit()
 
-         # except Exception as e:
-         # logger.error('Error Trying to Update DNS Record' + e.__str__())
-         # sys.exit()
-         # except Exception as e:
-         #     logger.error('Error Getting GoDaddy Records: ' + e.__str__())
 else:
-    logger.info(f'fDNS Record Update not Needed for :{externalIP}')
+    logger.info(f'fDNS Record Update not Needed for :{external_ip}')
 
 
 async def main():
     tasks = [
-        update_a_record(DOMAIN[1], externalIP)
+        update_a_record(DOMAIN[1], external_ip)
         for DOMAIN in DOMAINS_LIST
     ]
 
@@ -129,9 +144,18 @@ async def main():
 
 asyncio.run(main())
 
+# pending_tasks = asyncio.all_tasks()
+# for task in pending_tasks:
+#     logger.debug(f'Pending Task: {task.get_name()}')
+#     task.cancel()
+#     logger.debug(f'Pending Task Killed: {task.get_name()}')
+
 
  # TODO: Update CloudflareDynDNS.ini with new external IP address
 
 
 # TODO: Set CloudflareDynDNS.ini with error so will try to update Cloudflare again on next run
 logger.info("Code Executed in %s Seconds", (time.time() - start_time))
+
+
+
